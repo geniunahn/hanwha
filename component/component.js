297ac9d870b51533bf4cf46f9e3fbf36661a38bc
geniunahn <106502672+geniunahn@users.noreply.github.com>
2022-12-07 20:12:00 +0900

    Add files via upload
    헤더에 사이트맵 아이콘을 추가하고 사이트맵 이벤트를 구현함

--- component/component.js
+++ component/component.js
@@ -1,3 +1,5 @@
+// 헤더의 스크롤 이벤트입니다.
+
 $(function () {
   $(window).scroll(function () {
     let scrollT2 = $(window).scrollTop();
@@ -7,5 +9,17 @@ $(function () {
     } else {
       $(".header").removeClass("active");
     }
+  });
+});
+
+// 사이트맵 버튼 입니다.
+
+$(function () {
+  $(".site_map_btn").on("click", function () {
+    $(".site_map").toggleClass("site_map_open");
+  });
+
+  $(".site_map_close_btn img").on("click", function () {
+    $(".site_map").removeClass("site_map_open");
   });
 });
